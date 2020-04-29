60fd7ee49f8925c03ebf8ae5d07de3bcbc6fbc40
Stuart Chalmers <stuart.chalmers@gmail.com>
2020-04-29 23:34:51 +0100

    Added check for empty json file
    Checking if json returns any values as TFL not reporting some bus times during COVID-19 virus.

--- next_bus.py
+++ next_bus.py
@@ -10,42 +10,51 @@ import os
 # Query TFL API
 
 resp = requests.get('https://api.tfl.gov.uk/StopPoint/40004405165B/Arrivals')
+
 parsed_resp = json.loads(resp.text)
 
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+if len(parsed_resp) != 0:
 
-# Parse Reponse
     
-expectedArrival = dateutil.parser.parse(parsed_resp[0]['expectedArrival'])
-
-busNumberPlate = parsed_resp[0]['vehicleId']
-
-currentTime = datetime.now(pytz.timezone('Europe/London'))
-
-timeUntil = expectedArrival - datetime.now(pytz.timezone('Europe/London'))
-
-secs = timeUntil.total_seconds()
-hours = int(secs / 3600)
-minutes = int(secs / 60) % 60
-
-# Print details
-
-print('Current time : ',currentTime.strftime('%H:%M:%S'))
-print('Time until K3 bus (number plate ',busNumberPlate,'): ',minutes,'min\n')
-
-# Get to the bus stop!
-
-if(minutes < 10):
-    print("Hurry up to bus stop!\n")
-
-# Flash lights on Unicorn Hat on Raspberry Pi
     
-if(minutes == 10):
-	os.system('sudo python /home/pi/k3/bus_sparkles.py')
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 
-# Write k3 bus details to file
+    else:
+        # Parse Reponse
+    
+        expectedArrival = dateutil.parser.parse(parsed_resp[0]['expectedArrival'])
 
-with open("/home/pi/k3/timeUntil.txt", "w") as text_file:
-    text_file.write("{0},{1},{2}".format(minutes,busNumberPlate,currentTime.strftime('%H:%M:%S')))
+        busNumberPlate = parsed_resp[0]['vehicleId']
+
+        currentTime = datetime.now(pytz.timezone('Europe/London'))
+
+        timeUntil = expectedArrival - datetime.now(pytz.timezone('Europe/London'))
+
+        secs = timeUntil.total_seconds()
+        hours = int(secs / 3600)
+        minutes = int(secs / 60) % 60
+
+        # Print details
+
+        print('Current time : ',currentTime.strftime('%H:%M:%S'))
+        print('Time until K3 bus (number plate ',busNumberPlate,'): ',minutes,'min\n')
+
+        # Get to the bus stop!
+
+        if(minutes < 10):
+            print("Hurry up to bus stop!\n")
+
+            # Flash lights on Unicorn Hat on Raspberry Pi
+    
+        if(minutes == 10):
+	        os.system('sudo python /home/pi/k3/bus_sparkles.py')
+
+            # Write k3 bus details to file
+
+        with open("/home/pi/k3/timeUntil.txt", "w") as text_file:
+            text_file.write("{0},{1},{2}".format(minutes,busNumberPlate,currentTime.strftime('%H:%M:%S')))
+else:
+    print("No bus information available at this time")
+    os.system('sudo python /home/pi/k3/bus_sparkles.py')
